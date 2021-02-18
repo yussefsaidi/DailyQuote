6a52cb631c5fe34d996d00da1fd3eb58f8372829
Yussef <yussefsaidi@outlook.com>
2021-02-18 18:28:53 -0500

    Update application style

--- app/src/main/java/com/yussefsaidi/dailyquote/MainActivity.kt
+++ app/src/main/java/com/yussefsaidi/dailyquote/MainActivity.kt
@@ -16,7 +16,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
